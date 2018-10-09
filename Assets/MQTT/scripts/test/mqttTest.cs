9850d9a10c863acbf675f00601e22fe142889b35
noseeevil <oleksandrpatsora@gmail.com>
2018-10-09 16:44:01 +0300

    update

--- Assets/MQTT/scripts/test/mqttTest.cs
+++ Assets/MQTT/scripts/test/mqttTest.cs
@@ -8,40 +8,45 @@ using uPLibrary.Networking.M2Mqtt.Exceptions;
 
 using System;
 
-public class mqttTest : MonoBehaviour {
+public class mqttTest : MonoBehaviour 
+{
 	private MqttClient client;
-	// Use this for initialization
-	void Start () {
+
+    public string IpToParse = "10.0.123.12";
+
+    // Use this for initialization
+    public void Action () 
+    {
 		// create client instance 
-		client = new MqttClient(IPAddress.Parse("143.185.118.233"),8080 , false , null ); 
+		client = new MqttClient(IPAddress.Parse(IpToParse),8080 , false , null ); 
 		
 		// register to message received 
 		client.MqttMsgPublishReceived += client_MqttMsgPublishReceived; 
 		
-		string clientId = Guid.NewGuid().ToString(); 
-		client.Connect(clientId); 
+		string clientId = Guid.NewGuid().ToString();
+      
+        Debug.Log(clientId);
+
+        client.Connect(clientId); 
 		
 		// subscribe to the topic "/home/temperature" with QoS 2 
 		client.Subscribe(new string[] { "hello/world" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE }); 
 
 	}
-	void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) 
+
+    private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) 
 	{ 
 
 		Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message)  );
 	} 
 
-	void OnGUI(){
-		if ( GUI.Button (new Rect (20,40,80,20), "Level 1")) {
+	private void OnGUI()
+    {
+		if ( GUI.Button (new Rect (20,40,80,20), "Level 1")) 
+        {
 			Debug.Log("sending...");
 			client.Publish("hello/world", System.Text.Encoding.UTF8.GetBytes("Sending from Unity3D!!!"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
 			Debug.Log("sent");
 		}
-	}
-	// Update is called once per frame
-	void Update () {
-
-
-
 	}
 }
